4d3967066f0765a004e919667a612d6cfadb49a6
jdpye <jonathan.pye@dal.ca>
2021-03-30 22:56:07 -0300

    added tidied version of glatos-to-actel script

--- code/day2/glatos_data_in_actel.R
+++ code/day2/glatos_data_in_actel.R
@@ -21,8 +21,8 @@ receivers <- read_glatos_receivers(rec_file)
 # Don't try this at home, just use the workbook reader
 
 tags <- detections %>% 
-  group_by('animal_id') %>% 
-  select('animal_id', 'transmitter_codespace', 
+  dplyr::group_by('animal_id') %>% 
+  dplyr::select('animal_id', 'transmitter_codespace', 
          'transmitter_id', 'tag_type', 'tag_serial_number', 
          'common_name_e', 'capture_location', 'length', 
          'weight', 'sex', 'release_group', 'release_location', 
@@ -62,17 +62,17 @@ full_receiver_meta <- wb_metadata$receivers %>%
 actel_datefmt = '%Y-%m-%d %H:%M:%S'
 
 # biometrics is the tag metadata. If you have a tag metadata sheet, it looks like this:
-actel_biometrics <- wb_metadata$animals %>% mutate(Release.date = format(utc_release_date_time, actel_datefmt),
+actel_biometrics <- wb_metadata$animals %>% dplyr::mutate(Release.date = format(utc_release_date_time, actel_datefmt),
                                     Signal=as.integer(tag_id_code),
                                     Release.site = release_location) %>%
   # JON DEMO ONLY: subset these tag releases to the animals we actually have 
   # detections for in our demo dataset
   # Only doing this because the demo dataset is so cut-down, wouldn't make sense
   # to have 500 tags and only 3 of them with detections.
-                                    filter(animal_id %in% tags$animal_id)
+                                    dplyr::filter(animal_id %in% tags$animal_id)
 
 # deployments is based in the receiver deployment metadata sheet
-actel_deployments <- full_receiver_meta %>% filter(!is.na(recover_date_time)) %>%
+actel_deployments <- full_receiver_meta %>% dplyr::filter(!is.na(recover_date_time)) %>%
   mutate(Station.name = station,
          Start = format(deploy_date_time, actel_datefmt), # no time data for these deployments
          Stop = format(recover_date_time, actel_datefmt),  # not uncommon for this region
@@ -83,7 +83,7 @@ actel_deployments <- full_receiver_meta %>% filter(!is.na(recover_date_time)) %>
 #-------------------
 
 # Renaming some columns in the Detection extract files   
-actel_dets <- detections %>% mutate(Transmitter = transmitter_id,
+actel_dets <- detections %>% dplyr::mutate(Transmitter = transmitter_id,
                                     Receiver = as.integer(receiver_sn),
                                     Timestamp = format(detection_timestamp_utc, actel_datefmt),
                                     CodeSpace = extractCodeSpaces(transmitter_id),
@@ -92,11 +92,11 @@ actel_dets <- detections %>% mutate(Transmitter = transmitter_id,
 #-------------------
 
 # Prepare and style entries for receivers
-actel_receivers <- full_receiver_meta %>% mutate( Station.name = station,
+actel_receivers <- full_receiver_meta %>% dplyr::mutate( Station.name = station,
                                                   Latitude = deploy_lat,
                                                   Longitude = deploy_long,
                                                   Type='Hydrophone') %>%
-  mutate(Array=glatos_array) %>%    # Having this many distinct arrays breaks things with few clues as to why.
+  dplyr::mutate(Array=glatos_array) %>%    # Having this many distinct arrays breaks things with few clues as to why.
   dplyr::select(Station.name, Latitude, Longitude, Array, Type) %>%
   distinct(Station.name, Latitude, Longitude, Array, Type)
 
@@ -118,7 +118,7 @@ actel_spatial <- actel_receivers %>% bind_rows(actel_tag_releases)
 #-----------------------
 
 # group by station name and take the mean lat and lon of each station deployment history.
-actel_spatial_sum <- actel_spatial %>% group_by(Station.name, Type) %>%
+actel_spatial_sum <- actel_spatial %>% dplyr::group_by(Station.name, Type) %>%
   dplyr::summarize(Latitude = mean(Latitude),
                    Longitude = mean(Longitude),
                    Array =  first(Array))
@@ -142,8 +142,18 @@ actel_project <- preload(biometrics = actel_biometrics,
                          detections = actel_dets,
                          tz = tz)
 
+# Alas, we're going to have to discard a bunch of detections here, 
+# as our subsetted data doesn't have deployments for certain time periods
+# and is missing some station deployments altogether!
+
+c # discard detections outside of known deployment ranges
+e # discard all detections at unknown receivers - this is almost never
+  # what you want to do, but what choice do we have?
+
 # Get summary reports from our dataset:
-actel_explore_output <- explore(actel_project, report=TRUE, print.releases=FALSE, tz=tz)
+actel_explore_output <- explore(datapack=actel_project, 
+                                report=TRUE, 
+                                print.releases=FALSE)
 
 # Our analysis doesn't make a lot of sense, since...
 # actel assumed our study was linear, we didn't tell it otherwise!
@@ -159,8 +169,8 @@ library(leafpop)
 
 ## Exploration - Let's re-use mapview, since we're going to want to drill in and look at our stations
 
-our_receivers <- as.data.frame(actel_spatial_sum_lakes) %>%    
-  filter(Array %in% (actel_spatial_sum %>%   # Let's only look at the arrays already in our spatial file
+our_receivers <- as.data.frame(actel_spatial_sum) %>%    
+  dplyr::filter(Array %in% (actel_spatial_sum %>%   # Let's only look at the arrays already in our spatial file
                               distinct(Array))$Array)
 
 mapview(our_receivers %>%    
@@ -184,24 +194,36 @@ huron_arrays <- c('WHT', 'LVD', 'OSC', 'STG', 'PRS', 'FMP',
                   'GRS', 'LVU', 'TRN', 'BEI', 'MIS', 'TBA')
 
 
-# Bugged
-# Let's update our spatial to reflect the connectivity of the Huron arrays.
+# Let's update our spatial to reflect the inter-connectivity of the Huron arrays.
 actel_spatial_sum_lakes <- actel_spatial_sum %>% 
-    mutate(Array = if_else(Array %in% huron_arrays, 'Huron', #if any of the above, make it 'Huron'
+    dplyr::mutate(Array = if_else(Array %in% huron_arrays, 'Huron', #if any of the above, make it 'Huron'
                                        Array)) # else leave it as its current value
 
-# spatial_txt_dot = 'path/to/the/workshop/data/glatos_spatial.txt'
-spatial_txt_dot = 'C:/Users/Jon/Google Drive/PycharmProjects/workshops/temp/2021-03-30-glatos-workshop/data/glatos_spatial.txt'
+# Notice we haven't changed any of our data or metadata, just the spatial table
 
-actel_spatial_sum_lakes %>% group_by(Array) %>% select(Array) %>% unique()
+spatial_txt_dot = 'path/to/the/workshop/data/glatos_spatial.txt'
+
+# How many unique spatial Arrays do we have now that we've combined
+# so many into Huron?
+
+actel_spatial_sum_lakes %>% dplyr::group_by(Array) %>% dplyr::select(Array) %>% unique()
+
+
+# OK. let's analyze this dataset with our reduced spatial complexity
 
 actel_project <- preload(biometrics = actel_biometrics,
                          spatial = actel_spatial_sum_lakes,
                          deployments = actel_deployments,
                          detections = actel_dets,
-                         # dot = readr::read_file(spatial_txt_dot),
+                         dot = readLines(spatial_txt_dot),
                          tz = tz)
 
-actel_explore_output_lakes <- explore(actel_project, report=TRUE, print.releases=FALSE, tz=tz)
+# We still have our orphan detection issue
+c
+# And we still have receivers with detections but no deployment info
+e
+
+
+actel_explore_output_lakes <- explore(datapack=actel_project, report=TRUE, print.releases=FALSE)
 
 
