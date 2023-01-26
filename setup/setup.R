481752345e1617dd2950a5a026981c5a01d00b33
CaitlinBate <caitlin.bate@dal.ca>
2023-01-26 18:13:08 -0400

    edited setup.R

--- setup/setup.R
+++ setup/setup.R
@@ -3,7 +3,7 @@
 ### R version: 3.6.x or newer (recommend 4.0.x) and RStudio.
 ### Windows users: Please also download the Rtools version compatible with your R version https://cran.r-project.org/bin/windows/Rtools/history.html (not for MacOS)
 
-# For day 2 only: You must install also GDAL software, which can take a long time. See the extra setup document provided.
+# Advanced Telemetry Workshop ONLY: You must install also GDAL software, which can take a long time. See the extra setup document provided.
 
 # Once R/RStudio is installed: open RStudio and run this install script. Please run it line-by-line instead of all at once in case there are errors.
 
@@ -12,7 +12,7 @@
 
 
 
-## DAY 1 WORKSHOP REQUIREMENTS ----
+## Beginner R Workshop Requirements ----
 # Tidyverse (data cleaning and arrangement)
 install.packages('tidyverse')
 
@@ -34,8 +34,8 @@ install.packages("viridis")
 
 
 
-## DAY 2 WORKSHOP REQUIREMENTS ----
-#rgdal
+## Advanced Telemetry Workshop Requirements ----
+# rgdal
 install.packages('rgdal')
 library(rgdal)
 rgdal::getGDALVersionInfo()
@@ -43,15 +43,15 @@ rgdal::getGDALVersionInfo()
 # Tidyverse (data cleaning and arrangement)
 install.packages('tidyverse')
 
-# VTrack - Tools for Telemetry Analysis
-install.packages("devtools")
-library(devtools)
-devtools::install_github("rossdwyer/VTrack")
-
-# GLATOS - acoustic telemetry package that does filtering, vis, array simulation, etc.
+# glatos - acoustic telemetry package that does filtering, vis, array simulation, etc.
 install.packages('remotes')
-library(remotes)
+library(remotes) 
 remotes::install_github('ocean-tracking-network/glatos')
+
+#Additional packages for mapping.
+install.packages('stringr')
+install.packages('mapview')
+install.packages('spdplyr')
 
 # Lubridate - part of Tidyverse, improves the process of creating date objects
 install.packages('lubridate')
@@ -63,38 +63,42 @@ install.packages('ggmap')
 install.packages('sp')
 install.packages('raster')
 
-#Additional packages for mapping.
-install.packages('stringr')
-install.packages('mapview')
-install.packages('spdplyr')
-install.packages('rgdal')
-
 # Install actel
 library(remotes)
-remotes::install_github("hugomflavio/actel",
+remotes::install_github("hugomflavio/actel", 
                         build_opts = c("--no-resave-data", "--no-manual"), build_vignettes = TRUE)
 
-# Install packages for animating detection data
+# Install packages for building/displaying R Markdown
+install.packages('rmarkdown')
+install.packages('knitr', dependencies = TRUE)
 
-remotes::install_github("jmlondon/pathroutr")
-
-install.packages('sf')
-install.packages('plotly')
-install.packages('gganimate')
-install.packages('ggspatial')
-
-#Remora workshop requirements
+# Install additonal packages for `remora` lesson
 install.packages('readr')
 install.packages('sf')
 install.packages('stars')
+
+# Install remora
+install.packages('devtools')
+library(devtools)
 devtools::install_github('ocean-tracking-network/remora@get_data_qc', force=TRUE)
 
+# Install packages for animating detection data
+install.packages('remotes')
+library(remotes) 
+remotes::install_github("jmlondon/pathroutr")
+
+install.packages('plotly')
+install.packages('gganimate')
+install.packages('ggspatial')
+                                                            
+                                                            
+
 ### Dataset and Code -----
-# Once the packages are installed, you can download the datasets and code for this workshop from [workshop-git-URL].
+# Once the packages are installed, you can download the datasets and code for this workshop from https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.
 # 1) Select the GREEN "code" button at the top and choose "Download ZIP"
 # 2) Unzip the folder and move to secure location on your computer (Documents, Desktop etc.)
 # 3) Copy the folder's path and use it to set your working directly in R using `setwd('<path-to-folder>')`.
 
 # If you are familiar with Git and Github, feel free to clone this repository as you normally would,
-# by running `git clone [workshop-git-URL].git` in a terminal program
+# by running `git clone https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.git` in a terminal program
 # and following from step 3 above.
